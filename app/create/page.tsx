da81788028ecfefbb8b33d15204f7018012c798b
AmarnathSooraj <amarnathsooraj7575@gmail.com>
2025-11-29 19:06:52 +0530

    addd

---
+++ app/create/page.tsx
@@ -0,0 +1,78 @@
+'use client'
+import {useState} from 'react'
+
+export default function page(){
+
+  const [form,setForm] = useState({
+    partsNo:'',
+    dur:'10'
+  });
+  const [input,setInput]=useState('');
+
+  const handleChange=(e)=>{
+    const {name,value}=e.target;
+    setForm((prevData)=>({
+      ...prevData,
+      [name]:value,
+    })
+    )
+  } 
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    if(form.dur===''|| form.partsNo===''){
+      setInput('Fill the details');
+      return
+    }
+    setInput('');
+    console.log(form);
+  }
+   return (
+    <div className='min-h-screen bg-[#FAF8F1] flex items-center justify-center'>
+      <div className="flex flex-col min-h-[300px] w-[450px] md:w-[600px] p-6 rounded-2x shadow-2x border border-black/20" >
+        <p className='mb-4'>Room host as Amarnath Sooraj</p>
+
+        <form
+        onSubmit={handleSubmit}
+        className='w-full space-y-4'>
+          <div className='flex flex-col'>
+            <label htmlFor="ParticipantsNo">Number of Participants:</label>
+            <input type="number" min='0' max='20' className='border w-full border-black/60 p-2 my-2 outline-none'
+            name='partsNo'
+            value={form.partsNo}
+            onChange={handleChange}/>
+          </div>
+         <div className='flex flex-col'>
+            <label htmlFor="RoomTime">Room Duration:</label>
+            <select className='border my-2 p-2 outline-none' name='dur' value={form.dur} onChange={handleChange}>
+              <option value='10'>
+                10min
+              </option>
+              <option value='20'>
+                20min
+              </option>
+              <option value='30'>
+                30min
+              </option>
+              <option value='40'>
+                40min
+              </option>
+              <option value='50'>
+                50min
+              </option>
+              <option value='60'>
+                60min
+              </option>
+            </select>
+          </div>
+            {(input&&<p className='text-[#ce0000]'>{input}</p>)}
+          <div>
+            <button className='bg-black/85 text-white w-full p-2'>
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,56 @@
-import Image from "next/image";
+'use client'
 
+import Link from "next/link";
+import {useState} from 'react'
 export default function Home() {
+
+  const [sign,setSign] = useState(false);
+  const [input,setInput] = useState('');
+
+  let [formData,setForm] = useState({
+    fname:"",
+    email:"",
+    mobileNo:"",
+    password:"",
+    conpassword:""
+  })
+
+  const handleSign = ()=>{
+    setSign(!sign);
+    console.log('hello')
+  }
+
+  const handleChange = (e) =>{
+
+    const {name,value}= e.target;
+    setForm((prevData) => ({
+      ...prevData, // Spreads existing formData properties
+      [name]: value, // Overwrites the specific property that 
+    }));
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    console.log(formData);
+    if(formData.fname==''|| formData.email=='' || formData.mobileNo == '' || formData.password == '' || formData.conpassword == ''){
+      setInput('Fill all details');
+      return
+    }
+    if(formData.password===formData.conpassword){
+      return
+    }
+    setInput('Oops! Password do not match');
+  }
   return (
     <>
       <main className=" bg-[#FAF8F1]  px-4 md:px-10 w-full overflow-hidden relative">
-        <div className="md:hidden absolute top-8 bg-[#142572] text-white font-medium text-xl px-3">
+        <div className="md:hidden absolute top-8 bg-[#142572] text-white font-medium text-xl px-3 cursor-pointer" onClick={handleSign}>
           Sign Up
         </div>
         <div className=" text-black/85 flex flex-col md:justify-around place-content-center md:place-items-center min-h-screen md:flex-row w-full"> 
             {/* Left side */}
             <div className="md:max-w-[75%]">
-              <div className="space-y-5 md:space-y-6">
+              <div className="space-y-6 md:space-y-8">
                 <p className="text-7xl md:text-9xl font-medium">Chat<span className="bg-[#142572] text-white">Ups</span>.</p>
                 <p className="font-josefin text-lg md:text-xl">
                   "A fast, secure chat app that keeps you connected to the people
@@ -18,40 +58,98 @@ export default function Home() {
                 </p>
 
                 <div className="flex text-md md:text-lg">
-                  <button className="mr-8 py-3 px-6 bg-black/85 rounded-xs text-white">
+                  <Link href='/join' className="mr-8 py-3 px-6 bg-black/85 rounded-xs text-white">
                     Join Now
-                  </button>
-                  <button className="mr-8 py-3 px-6 bg-black/85 rounded-xs text-white">
+                  </Link>
+                  <Link href='/create' className="mr-8 py-3 px-6 bg-black/85 rounded-xs text-white">
                     Create a Room
-                  </button>
+                  </Link>
                 </div>
               </div>
             </div>
 
             {/* Right side form */}
             <div className="hidden md:block min-w-[30%]">
-              <form className="space-y-6">
+              <form
+              onSubmit={handleSubmit}
+              className="space-y-5">
                 <div className="flex border-b">
-                  <input className=" w-full p-2 focus-none outline-none" type="text" placeholder="Name"/>
+                  <input
+                    className=" w-full p-2 focus-none outline-none"
+                    type="text"
+                    onChange={handleChange}
+                    value={formData.fname}
+                    name="fname"
+                    placeholder="Name"/>
                 </div>
                 <div className="flex border-b">
-                  <input className=" w-full p-2 focus-none outline-none" type="email" placeholder="Email"/>
+                  <input className=" w-full p-2 focus-none outline-none" type="email" placeholder="Email"
+                    value={formData.email}
+                    name='email'
+                   onChange={handleChange}
+                  />
                 </div>
                  <div className="flex border-b">
-                  <input className=" w-full p-2 focus-none outline-none" type="email" placeholder="Mobile No"/>
+                  <input className=" w-full p-2 focus-none outline-none" type="text"
+                    value={formData.mobileNo}
+                    name='mobileNo'
+                    onChange={handleChange}
+                   placeholder="Mobile No"/>
                 </div>
                 <div className="flex border-b">
-                  <input className=" w-full p-2 focus-none outline-none" type="password" placeholder="Password"/>
+                  <input className=" w-full p-2 focus-none outline-none" type="password"
+                    name='password'
+                    value={formData.password}
+                   onChange={handleChange}
+                   placeholder="Password"/>
                 </div>
                 <div className="flex border-b">
-                  <input className=" w-full p-2 focus-none outline-none" type="password" placeholder="Confirm Password"/>
+                  <input className=" w-full p-2 focus-none outline-none" type="password" 
+                  name='conpassword'
+                  value={formData.conpassword}
+                   onChange={handleChange}
+                  placeholder="Confirm Password"/>
                 </div>
+
+                <div>
+                  {(input && <p className='mb-1 text-[#d60404c4] font-normal'>{input}</p>)}
+                <Link href='#' className="mr-4">Forgot password ?</Link>
+                <Link href='#'>Already have an account ?</Link>
+                </div>
+
                 <div className="flex place-content-end">
-                  <button className="text-white bg-black/85 px-4 py-2">Sign Up</button>
+                  <button className="text-white bg-black/85 px-4 py-2" >Sign Up</button>
                 </div>
               </form>
             </div>
           </div>
+
+
+          {/* {(sign&&
+          <div className="z-200 absolute top-0 w-full bg-white/90 left-0 max-w-[80%] p-4 rouned-sm shadow-md">
+            <div onClick={handleSign} className="cursor-pointer">X</div>
+            <form className="space-y-6">
+                  <div className="flex border-b">
+                    <input className=" w-full p-2 focus-none outline-none" type="text" placeholder="Name"/>
+                  </div>
+                  <div className="flex border-b">
+                    <input className=" w-full p-2 focus-none outline-none" type="email" placeholder="Email"/>
+                  </div>
+                  <div className="flex border-b">
+                    <input className=" w-full p-2 focus-none outline-none" type="email" placeholder="Mobile No"/>
+                  </div>
+                  <div className="flex border-b">
+                    <input className=" w-full p-2 focus-none outline-none" type="password" placeholder="Password"/>
+                  </div>
+                  <div className="flex border-b">
+                    <input className=" w-full p-2 focus-none outline-none" type="password" placeholder="Confirm Password"/>
+                  </div>
+                  <div className="flex place-content-end">
+                    <button className="text-white bg-black/85 px-4 py-2">Sign Up</button>
+                  </div>
+              </form>
+            </div>
+          )} */}
       </main>
     </>
   );
